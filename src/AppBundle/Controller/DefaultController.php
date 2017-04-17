0ca62687d74b420aa19f6dd4aa6701a1aedd5979
VincentTim <rougiervincent@gmail.com>
2017-04-17 21:40:56 +0200

    curl request instgram api

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use InstagramAPI\Instagram;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +20,37 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(){
+
+        //https://api.instagram.com/v1/users/self/media/recent/?limit=70&access_token=1532167946.1b47213.964c653517c446bf899d3067c321a110
+
+        $uri = 'https://api.instagram.com/v1/';
+        $endpoint = 'users/self/media/recent/';
+        $token = '1532167946.1b47213.964c653517c446bf899d3067c321a110';
+
+        // Get cURL resource
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_VERBOSE => true,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $uri . $endpoint . $token,
+            CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+            CURLOPT_SSL_VERIFYPEER => false
+        ));
+// Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        var_dump($resp);
+        echo curl_error($curl);
+// Close request to clear up some resources
+        curl_close($curl);
+
+        return new Response(1);
+
     }
 }
